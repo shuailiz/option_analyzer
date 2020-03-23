eccb2713b80778876f313cda59f04d8b336e678b
Shuai <shuailirpi@gmail.com>
2020-03-22 22:35:58 -0700

    Bug fixes for fetch ticker data script

--- scripts/fetch_ticker_data.py
+++ scripts/fetch_ticker_data.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 
-base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))) #change the file name here
+base_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) #change the file name here
 sys.path.append(base_path)
 from data_utils.data_utils import DataManager
 
@@ -18,13 +18,14 @@ data_path = os.path.join(base_path, 'data')
 ticker_file = 'Ticker_List.csv'
 ticker_list = pd.read_csv(os.path.join(data_path,ticker_file), header = 0)
 
-ticker = ticker_list['Ticker'].to_numpy()
+tickers = ticker_list['Ticker'].to_numpy()
 
 
 def download_data(ticker):
 	data_manager = DataManager(api="alpha_vantage")
-	imported_data = data_manager.get_historical_data(symbol=symbol_string,
-                                                     interval='daily')
+	imported_data = data_manager.get_historical_data(symbol=ticker,
+                                                         interval='daily',
+                                                         save_fetched=True)
 
-for i in (len(ticker)):
-	download_data(ticker[i])
+for ticker in tickers:
+	download_data(ticker)
